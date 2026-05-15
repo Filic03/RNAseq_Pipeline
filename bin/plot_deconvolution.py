7b82361530f0769ebdc0629048493afd748b9b22
FeliceDiCasola <felicedicasola3@gmail.com>
2026-05-15 16:32:23 +0200

    Update plot_deconvolution.py

--- bin/plot_deconvolution.py
+++ bin/plot_deconvolution.py
@@ -8,27 +8,18 @@ input_excel = sys.argv[1]
 
 print(f"Lettura dei risultati da {input_excel}...")
 df = pd.read_excel(input_excel, index_col=0)
-
-
 df_t = df.T
 
 print("Generazione dello Stacked Barplot...")
-
 fig, ax = plt.subplots(figsize=(12, 8))
-
-
 df_t.plot(kind='bar', stacked=True, ax=ax, colormap='tab20', width=0.8)
-
-
 plt.legend(title='Cell Types (ImmuCellAI v2)', bbox_to_anchor=(1.02, 1), loc='upper left', fontsize=8, ncol=2)
-
-
 plt.title("Tumor Microenvironment (TME) Cellular Composition", fontsize=16, fontweight='bold', pad=20)
 plt.ylabel("Relative Abundance (Fraction)", fontsize=12, fontweight='bold')
 plt.xlabel("Samples", fontsize=12, fontweight='bold')
 plt.xticks(rotation=45, ha='right')
 plt.tight_layout()
-plt.savefig(output_file, dpi=300, bbox_inches='tight')
+plt.savefig("TME_Stacked_Barplot.pdf", dpi=300, bbox_inches='tight')
 plt.close()
 
 #PLOT 2
@@ -62,5 +53,3 @@ plt.title("Cell Type Distribution across all samples", fontsize=15, fontweight='
 plt.tight_layout()
 plt.savefig("TME_Abundance_Boxplot.pdf", dpi=300, bbox_inches='tight')
 plt.close()
-
-output_file = "ImmuCellAI_plots.pdf"
